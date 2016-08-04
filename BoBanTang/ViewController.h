6a9ef777f403047d65e3bc939f8b1fa82817767f
Authority01 <370850377@qq.com>
2016-08-04 21:03:28 +0800

    Initial Commit

---
+++ BoBanTang/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BoBanTang
+//
+//  Created by Authority on 16/8/4.
+//  Copyright © 2016年 Authority. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BoBanTang/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BoBanTang
+//
+//  Created by Authority on 16/8/4.
+//  Copyright © 2016年 Authority. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
